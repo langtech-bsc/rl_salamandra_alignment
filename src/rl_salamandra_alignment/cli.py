d0cedd2ac794fe8b804a73b58534f586e2c66182
Luis Vasquez <luis.vasquez@bsc.es>
2025-01-28 22:15:00 +0100

    Implemente first working version of CLI

--- src/rl_salamandra_alignment/cli.py
+++ src/rl_salamandra_alignment/cli.py
@@ -1,21 +1,24 @@
 """Console script for rl_salamandra_alignment."""
 import rl_salamandra_alignment
-
-import typer
-from rich.console import Console
-
-app = typer.Typer()
-console = Console()
+from argparse import ArgumentParser
+from rl_salamandra_alignment.fill_template import generate_all_job_files, try_load_config
 
 
-@app.command()
 def main():
-    """Console script for rl_salamandra_alignment."""
-    console.print("Replace this message by putting your code into "
-               "rl_salamandra_alignment.cli.main")
-    console.print("See Typer documentation at https://typer.tiangolo.com/")
-    
+    """Reinforcement Learning for Salamandra on MN5"""
+    parser = ArgumentParser()
+    parser.add_argument(
+        "config",
+        help="YAML Config file for RL")
+    args = parser.parse_args()
+
+    config = try_load_config(args.config)
+    all_job_files = generate_all_job_files(config)
+    for distributed_run_script, launch_script in all_job_files:
+        print("="*10)
+        print(f"Distributed execution script:\n{distributed_run_script}")
+        print(f"Launch script:\n{launch_script}")
 
 
 if __name__ == "__main__":
-    app()
+    main()
