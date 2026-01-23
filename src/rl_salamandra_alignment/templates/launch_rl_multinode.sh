90e18cf2e36a10346f1aa01deefcec741a7bee77
Luis Vasquez <luis.vasquez@bsc.es>
2026-01-23 11:55:49 +0100

    Copy original safetensors.index, this fixes 'total_parameters' mismatch

--- src/rl_salamandra_alignment/templates/launch_rl_multinode.sh
+++ src/rl_salamandra_alignment/templates/launch_rl_multinode.sh
@@ -142,6 +142,7 @@ torchrun "${torchrun_distributed_args[@]}" \
 
 # copy original tokenizer config to get the original chat_template
 cp $ORIGINAL_MODEL_PATH/tokenizer_config.json $TRAINING_OUTPUT_DIR/tokenizer_config.json 
+cp $ORIGINAL_MODEL_PATH/model.safetensors.index.json $TRAINING_OUTPUT_DIR/model.safetensors.index.json
 # clean up
 
 chmod --recursive 770 $TRAINING_OUTPUT_DIR # Make sure the group also has access
